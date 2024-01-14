5d0f941b4da58782b70a26b7e2bb1ed7eee4b51d
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-14 11:10:04 +0530

    inititla

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000");
+})
