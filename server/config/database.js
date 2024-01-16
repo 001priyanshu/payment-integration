d21e0d84eee4d1e4e7bb74214e88c2ee83c96ae4
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-16 11:36:36 +0530

    database

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-export const connectDB = async () => {
+exports.connectDB = async () => {
   const { connection } = await mongoose.connect(process.env.MONGO_URI);
   console.log(`Mongodb is connected with ${connection.host}`);
 };
